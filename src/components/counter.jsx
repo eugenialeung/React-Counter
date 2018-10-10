71382e6a93273f46ca9508537fe7902190ac791a
Eugenia Leung <eugenia_leung316@yahoo.com>
2018-10-10 14:56:49 -0700

    refactored clicker count

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,8 +17,9 @@ class Counter extends Component {
   //     this.handleIncrement = this.handleIncrement.bind(this);
   //   }
 
-  handleIncrement = () => {
-    console.log("Increment Clicked", this);
+  handleIncrement = product => {
+    // console.log("Increment Clicked", this);
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -33,7 +34,12 @@ class Counter extends Component {
     );
   }
 
+  //   doHandleIncrement = () => {
+  //     this.handleIncrement({ id: 1 });
+  //   };
+
   render() {
+    const product = { id: 1 };
     return (
       <React.Fragment>
         {/* <img src={this.state.imageUrl} alt="" /> */}
@@ -41,7 +47,7 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
